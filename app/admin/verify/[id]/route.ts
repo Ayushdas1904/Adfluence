4e6703304e68cf6d67426f9ee338c5f02553e400
Ayush Das <ayushdas1904@gmail.com>
2025-06-15 13:37:28 +0530

    fix: Business Verify

--- app/admin/verify/[id]/route.ts
+++ app/admin/verify/[id]/route.ts
@@ -1,14 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
 import { connectToDatabase } from "@/lib/db";
 import Business from "@/models/business";
-import { NextRequest } from "next/server";
 
 export async function PATCH(
   req: NextRequest,
   { params }: { params: { id: string } }
-) {
+): Promise<NextResponse> {
   await connectToDatabase();
 
   await Business.findByIdAndUpdate(params.id, { verified: true });
 
-  return new Response("Business verified successfully", { status: 200 });
+  return NextResponse.json({ message: "Business verified successfully" });
 }
